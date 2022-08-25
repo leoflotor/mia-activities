f44d6a5f81abe3335ca3002cffd87ea73eebf963
leoflotor <leonardoflotor@gmail.com>
2022-08-25 01:04:42 -0500

    Simple hello world script in lisp

---
+++ mia-2022-half2/pia/hello.lisp
@@ -0,0 +1,13 @@
+#!/usr/local/bin/sbcl --script
+;;;; Hello world
+
+;;; Simple script that prints a greeting.
+
+;;; The manual of SBCL can be found here:
+;;; https://www.sbcl.org/manual/index.html
+
+;;; A simple explanation of semicolon comments convention:
+;;; https://stackoverflow.com/questions/6365334/lisp-commenting-convention
+
+(write-line "Hello, World!")
+(write-line "Hello, World!")
