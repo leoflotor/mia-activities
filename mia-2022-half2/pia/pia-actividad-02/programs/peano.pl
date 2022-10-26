7df4ef7bfe37d48ffc1be75abb6d769b42e7e368
leoflotor <leonardoflotor@gmail.com>
2022-10-26 17:51:36 -0500

    Checkpoint, mainly on pia assignment.

--- mia-2022-half2/pia/pia-actividad-02/programs/peano.pl
+++ mia-2022-half2/pia/pia-actividad-02/programs/peano.pl
@@ -19,8 +19,13 @@ peanoToNat_fd(S, N) :-
 % X = s(s(s(0))) .
 natToPeano(0, 0).
 natToPeano(N, s(X)) :-
-    N > 0,
+    N >= 0,
     N1 is N - 1,
+    natToPeano(N1, X).
+%%% natToPeano/2 also deals with negative natural numbers
+natToPeano(N, -s(X)) :-
+    N < 0,
+    N1 is - N - 1,
     natToPeano(N1, X).
 
 %%% peanoToNat/2 computes the respective natural number of a Peano number
@@ -32,7 +37,31 @@ peanoToNat(s(X), N) :-
     peanoToNat(X, N1),
     N is N1 + 1.
 
+%%% peanoToNat/2 also deals with negative Peano numbers
+peanoToNat(-s(X), -N) :-
+    peanoToNat(X, N1),
+    N is N1 + 1.
+
 %%% addPeano/3 adds S2 to S1, S3 = S1 + S2
+% addPeano(S1, S2, S3) :-
+%     peanoToNat(S1, N1),
+%     peanoToNat(S2, _),
+%     N1 < 0,
+%     substractPeano(S1, S2, S3).
+
+% addPeano(S1, S2, S3) :-
+%     peanoToNat(S1, _),
+%     peanoToNat(S2, N2),
+%     N2 < 0,
+%     substractPeano(S1, S2, S3).
+
+% addPeano(S1, S2, S3) :-
+%     peanoToNat(S1, N1),
+%     peanoToNat(S2, N2),
+%     N3 is N1 + N2,
+%     N3 < 0,
+%     natToPeano(N3, S3).
+
 addPeano(S1, S2, S3) :-
     peanoToNat(S1, N1),
     peanoToNat(S2, N2),
@@ -43,9 +72,17 @@ addPeano(S1, S2, S3) :-
 substractPeano(S1, S2, S3) :-
     peanoToNat(S1, N1),
     peanoToNat(S2, N2),
-    N1 > N2,
+    N1 >= N2,
     N3 is N1 - N2,
     natToPeano(N3, S3).
+
+substractPeano(S1, S2, -S3) :-
+    peanoToNat(S1, N1),
+    peanoToNat(S2, N2),
+    N1 < N2,
+    N3 is N2 - N1,
+    natToPeano(N3, S3).
+
 
 
 % USEFUL LINKS
